767a881c84f2f3e2451928d5bf9ef03fb65d5288
Minoli Fernando <44726915+Minofdo@users.noreply.github.com>
2022-04-13 16:13:11 +0530

    Add files via upload

---
+++ Nibmbroker.swift
@@ -0,0 +1,17 @@
+//
+//  adappApp.swift
+//  adapp
+//
+//  Created by Maheesha on 4/11/22.
+//
+
+import SwiftUI
+
+@main
+struct adappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
